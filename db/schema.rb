80ad9da7bb483c9d6fd5062f8bc86243066ef2e4
Barry Roberts <barryjroberts@gmail.com>
2023-08-14 18:45:43 +0900

    Thanks Doug

--- db/schema.rb
+++ db/schema.rb
@@ -10,46 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_14_084138) do
+ActiveRecord::Schema[7.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "bookings", force: :cascade do |t|
-    t.string "start_date"
-    t.string "end_date"
-    t.string "status"
-    t.bigint "user_id", null: false
-    t.bigint "ship_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["ship_id"], name: "index_bookings_on_ship_id"
-    t.index ["user_id"], name: "index_bookings_on_user_id"
-  end
-
-  create_table "ships", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.integer "capacity"
-    t.float "price_per_day"
-    t.string "ship_class"
-    t.string "ship_origin"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_ships_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.float "rating"
-    t.string "phone_number"
-    t.string "address"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "bookings", "ships"
-  add_foreign_key "bookings", "users"
-  add_foreign_key "ships", "users"
 end
