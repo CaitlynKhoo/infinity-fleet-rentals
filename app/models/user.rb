7ed5a86c3b4809b5c2d0001e7846cc2fa0421ba9
Kostas Piludis <kostas.piludis@gmail.com>
2023-08-14 17:59:04 +0900

    implemented devise and added required user fields to migration

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :date_of_birth, presence: true
+  validates :phone_number, presence: true
+end
