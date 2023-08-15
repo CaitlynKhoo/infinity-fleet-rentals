e2b350ba9db71f231426808d8c84b40c09a2f52e
Barry Roberts <barryjroberts@gmail.com>
2023-08-15 11:57:35 +0900

    seeds updated

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,5 +3,4 @@ class Booking < ApplicationRecord
   belongs_to :ship
   validates :start_date, presence: true
   validates :end_date, presence: true
-  validates_numericality_of :end_date, :greater_than => Proc.new { |booking| booking.start_date }
 end
--- db/migrate/20230814084138_create_bookings.rb
+++ db/migrate/20230814084138_create_bookings.rb
@@ -1,8 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[7.0]
   def change
     create_table :bookings do |t|
-      t.string :start_date
-      t.string :end_date
+      t.date :start_date
+      t.date :end_date
       t.string :status
       t.references :user, null: false, foreign_key: true
       t.references :ship, null: false, foreign_key: true
