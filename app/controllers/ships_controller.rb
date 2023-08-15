04d2f46bf6e0f248dd822d0dd4adaaf273330401
Barry Roberts <barryjroberts@gmail.com>
2023-08-15 18:23:48 +0900

    work in progress - my ships page still incomplete

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -9,4 +9,14 @@ class ShipsController < ApplicationController
     @booking = Booking.new
   end
 
+  def user_index
+    # if current_user.nil?
+    #   redirect_to ships_path
+    # else
+    # @ships = Ship.where(user_id: current_user.id)
+    @user = User.find(1)
+    @ships = Ship.where(user_id: 1)
+    # end
+  end
+
 end
