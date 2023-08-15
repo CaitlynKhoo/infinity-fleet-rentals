ecf8f9b2e94a4c132f08faef56f5e0d87dab81f8
Kostas Piludis <kostas.piludis@gmail.com>
2023-08-15 12:35:11 +0900

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :spaceships, only: [:index, :show, :new, :create] do
+    resources :bookings, only: [:create]
+  end
+
+  resources :bookings, only: [:update, :index ]
+  get 'my_spaceships', to: 'spaceships#user_index'
 end
