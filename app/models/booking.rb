d3c8d0fbe8c6745403b61e6dd48a01628a2c2b3c
karthika sanal <karthikasanal20@gmail.com>
2023-08-17 22:28:12 +1000

    shipview style changes and flatpicker.js

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,7 @@ class Booking < ApplicationRecord
   belongs_to :ship
   validates :start_date, presence: true
   validates :end_date, presence: true
+
+  validates :end_date, comparison: { greater_than: :start_date, message: "must follow start date." }
+  validates :start_date, comparison: { greater_than: Date.today, message: "must be today or later." }
 end
